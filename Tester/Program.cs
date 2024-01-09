3811d372c1284461a55a0011f7374af876fe8cde
M5kwc1m <144521687+mzkwcim@users.noreply.github.com>
2024-01-09 21:46:23 +0100

    Update Program.cs
    I've made some minor changes so now i can choose place to seat

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,15 +1,38 @@
 
 string[] y = new string[4];
 string[] x = new string[4];
-Drawer.DrawBoard();
-Console.SetCursorPosition(0, 0);
-Highlighter.Highlight(x, y);
-
+List<string> seatList = Initializer.SeatListCreater(y, x);
+Drawer.DrawBoard(seatList);
+Console.SetCursorPosition(0,0);
+StateChanger.Changer(seatList, x, y);
+class Initializer
+{
+    public static List<string> SeatListCreater(string[] x, string[] y)
+    {
+        List<string> SeatList = new List<string>();
+        for (int i = 0; i < x.Length * y.Length; i++)
+        {
+            SeatList.Add("O");
+        }
+        return SeatList;
+    }
+}
+class StateChanger
+{
+    public static void Changer(List<string> seatList, string[] x, string[] y)
+    {
+        int[] tab = Highlighter.Highlight(x, y);
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Clear();
+        seatList[(tab[1] * x.Length) + tab[0]] = "X";
+        Drawer.DrawBoard(seatList);
+    }
+}
 class MoveCursor
 {
     public static void MoveUp(string[] y) => Console.CursorTop = (Console.CursorTop - 1 < 0) ? y.Length - 1 : (Console.CursorTop - 1) % (y.Length - 1);
     public static void MoveDown(string[] y) => Console.CursorTop = (Console.CursorTop + 1 > 3) ? 0 : (Console.CursorTop + 1) % y.Length;
-    public static void MoveLeft(string[] x) => Console.CursorLeft = (Console.CursorLeft - 1 < 0) ? x.Length-1 : Console.CursorLeft - 1;
+    public static void MoveLeft(string[] x) => Console.CursorLeft = (Console.CursorLeft - 1 < 0) ? x.Length - 1 : Console.CursorLeft - 1;
     public static void MoveRight(string[] x) => Console.CursorLeft = (Console.CursorLeft + 1 > 3) ? 0 : (Console.CursorLeft + 1) % x.Length;
 }
 class Coloring
@@ -29,50 +52,46 @@ class Coloring
 }
 class MoveAndColor
 {
-    public static void MoveUpAndColor(string[] y )
+    public static void MoveUpAndColor(string[] y)
     {
-        Coloring.UnColor(Console.CursorLeft,Console.CursorTop);
+        Coloring.UnColor(Console.CursorLeft, Console.CursorTop);
         MoveCursor.MoveUp(y);
-        Coloring.Color(Console.CursorLeft%y.Length, Console.CursorTop);
+        Coloring.Color(Console.CursorLeft % y.Length, Console.CursorTop);
     }
     public static void MoveDownAndColor(string[] y)
     {
         Coloring.UnColor(Console.CursorLeft, Console.CursorTop);
         MoveCursor.MoveDown(y);
-        Coloring.Color(Console.CursorLeft%y.Length, Console.CursorTop);
+        Coloring.Color(Console.CursorLeft % y.Length, Console.CursorTop);
     }
     public static void MoveRightAndColor(string[] x)
     {
         Coloring.UnColor(Console.CursorLeft, Console.CursorTop);
         MoveCursor.MoveRight(x);
-        Coloring.Color(Console.CursorLeft%x.Length, Console.CursorTop);
+        Coloring.Color(Console.CursorLeft % x.Length, Console.CursorTop);
     }
     public static void MoveLeftAndColor(string[] x)
     {
         Coloring.UnColor(Console.CursorLeft, Console.CursorTop);
         MoveCursor.MoveLeft(x);
-        Coloring.Color(Console.CursorLeft%x.Length, Console.CursorTop);
+        Coloring.Color(Console.CursorLeft % x.Length, Console.CursorTop);
     }
 }
 class Drawer
 {
-    public static void DrawBoard()
+    public static void DrawBoard(List<string> SeatList)
     {
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < SeatList.Count; i++)
         {
-            for (int j = 0; j < 4; j++)
-            {
-                Console.Write("O");
-            }
-            Console.WriteLine();
+            Console.Write((i%4 == 3) ?SeatList[i] +"\n" : SeatList[i]);
         }
     }
 }
 class Highlighter
 {
-    public static int Highlight(string[] x, string[] y)
+    public static int[] Highlight(string[] x, string[] y)
     {
-        int number = 0;
+        int[] number = new int[2];
         ConsoleKey key;
         do
         {
@@ -83,7 +102,7 @@ class Highlighter
                 case ConsoleKey.DownArrow: MoveAndColor.MoveDownAndColor(y); break;
                 case ConsoleKey.RightArrow: MoveAndColor.MoveRightAndColor(x); break;
                 case ConsoleKey.LeftArrow: MoveAndColor.MoveLeftAndColor(x); break;
-                case ConsoleKey.Enter: number = Console.CursorTop; break;
+                case ConsoleKey.Enter: number = [Console.CursorLeft,Console.CursorTop]; break;
             }
         } while (key != ConsoleKey.Enter);
         return number;
